092bd8d35dd54d7bf551201235a63e43e318434f
pdomokos <domokospeter@gmail.com>
2022-05-17 20:05:04 +0200

    Add more test case

---
+++ src/main/java/com/example/webfluxtest/config/HttpClientConfig.java
@@ -0,0 +1,27 @@
+package com.example.webfluxtest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpClient;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class HttpClientConfig {
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .executor(Executors.newSingleThreadExecutor())
+                .build();
+    }
+
+    @Bean
+    public HttpClient httpClient2() {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+    }
+
+}
--- src/main/java/com/example/webfluxtest/controller/TestController.java
+++ src/main/java/com/example/webfluxtest/controller/TestController.java
@@ -1,12 +1,19 @@
 package com.example.webfluxtest.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,10 +21,62 @@ public class TestController {
 
     private final WebClient webClient;
 
+    @Qualifier("httpClient")
+    private final HttpClient httpClient;
+
+    @Qualifier("httpClient2")
+    private final HttpClient httpClient2;
+
     @GetMapping(value = "/webflux-webclient-test")
-    public Mono<String> getTestUsingWebfluxWebclient() {
-        return webClient.get().uri(URI.create("http://192.168.0.59:8080/v1/get-test-string"))
+    public Mono<String> getTestUsingWebfluxWebclient(@RequestParam long delay) {
+        return webClient.get().uri("http://192.168.0.59:8080/v1/get-test-string-1?delay={delay}", delay)
                 .retrieve()
                 .bodyToMono(String.class);
+    }
+
+    @GetMapping(value = "/springboot-httpclient-test")
+    public String getTestUsingHttpClient(@RequestParam long delay) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(String.format("http://192.168.0.59:8080/v1/get-test-string-2?delay=%d", delay)))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+
+    @GetMapping(value = "/springboot-httpclient-async-test")
+    public CompletableFuture<String> getTestAsyncUsingHttpClient(@RequestParam long delay) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(String.format("http://192.168.0.59:8080/v1/get-test-string-2?delay=%d", delay)))
+                .build();
+
+        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body);
+    }
+
+    @GetMapping(value = "/springboot-httpclient-test-2")
+    public String getTestUsingHttpClient2(@RequestParam long delay) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(String.format("http://192.168.0.59:8080/v1/get-test-string-2?delay=%d", delay)))
+                .build();
+
+        HttpResponse<String> response = httpClient2.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+
+    @GetMapping(value = "/springboot-httpclient-async-test-2")
+    public CompletableFuture<String> getTestAsyncUsingHttpClient2(@RequestParam long delay) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(String.format("http://192.168.0.59:8080/v1/get-test-string-2?delay=%d", delay)))
+                .build();
+
+        return httpClient2.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body);
     }
 }
